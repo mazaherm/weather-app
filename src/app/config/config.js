aaa8a113a0a9f593c9dd840e9d8bc5232c276504
Mazaher Muraj <mmuraj.313@gmail.com>
2020-11-13 18:10:10 +0000

    hide api key

--- src/app/config/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  API_KEY: '51817be98feb7b542e2a2973fa782725'
-}
-
-export default config
--- src/app/services/CallWeatherAPI/CallWeatherAPI.js
+++ src/app/services/CallWeatherAPI/CallWeatherAPI.js
@@ -1,14 +1,14 @@
-import axios from 'axios'
-import config from '../../config'
+import axios from "axios";
+import config from "../../config";
 
-const API_KEY = config.API_KEY
-const URL = `https://api.openweathermap.org/data/2.5/forecast?q=London,UK&units=metric&cnt=1&appid=${API_KEY}`
+const API_KEY = config.API_KEY;
+const URL = `https://api.openweathermap.org/data/2.5/forecast?q=London,UK&units=metric&cnt=1&appid=${API_KEY}`;
 
 export default async () => {
   try {
-    const response = await axios.get(URL)
-    return response.data
+    const response = await axios.get(URL);
+    return response.data;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
